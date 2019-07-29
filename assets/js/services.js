1b2f70e9d2ee490cb38251ee2b6245c9e3bf3360
un1qu31d <un1qu31d@gmail.com>
2019-07-29 20:39:34 +0200

    201907292039

---
+++ assets/js/services.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', e => {
+  // console.log('loading');
+  document.querySelector('body').classList.add('status--animation-start');
+  // document.querySelectorAll('.service').forEach((service, serviceIndex) => {
+
+  // });
+});
